de820242901a568433803b9e53edc499b1750686
LucaSalmi <luca.salmi90@gmail.com>
2021-09-20 13:20:36 +0200

    added int i

--- app/src/main/java/com/khystudent/homeworks/MainActivity.java
+++ app/src/main/java/com/khystudent/homeworks/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int i = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
